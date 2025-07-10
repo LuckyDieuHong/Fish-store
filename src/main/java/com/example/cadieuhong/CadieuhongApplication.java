5222a03711882311cbc645f6becb66c04ff5739f
Nguyen Hong Minh Quan <nguyenhongquan20042005@gmail.com>
2025-07-10 13:19:31 +0700

    Root project

---
+++ src/main/java/com/example/cadieuhong/CadieuhongApplication.java
@@ -0,0 +1,13 @@
+package com.example.cadieuhong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadieuhongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadieuhongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cadieuhong/CadieuhongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cadieuhong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadieuhongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
